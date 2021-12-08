05fdc9c7961b616eca9315614a4ccdb1ab86e558
shimamatz@gmail.com <shimamatz@gmail.com>
2021-12-09 00:57:51 +0900

    ダミーデータを記事モデルから取得したデータを取得するように修正

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,44 +1,14 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Article;
 
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
 {
     public function index(){
-        $articles = [
-            (object)[
-                'id' => 1,
-                'title' => 'タイトル１',
-                'body' => '本文1',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーサー名１',
-                ],
-            ],
-            (object)[
-                'id' => 2,
-                'title' => 'タイトル2',
-                'body' => '本文2',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーサー名2',
-                ],
-            ],
-            (object)[
-                'id' => 3,
-                'title' => 'タイトル3',
-                'body' => '本文3',
-                'created_at' => now(),
-                'user' => (object)[
-                    'id' => 1,
-                    'name' => 'ユーサー名3',
-                ],
-            ],
-        ];
+        $articles = Article::all()->sortByDesc('created_at');
         return view('articles.index', ['articles' => $articles]);
     }
 }
