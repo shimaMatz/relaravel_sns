c891c2faefeb24fff645295facc35295aadebaf1
shimamatz@gmail.com <shimamatz@gmail.com>
2021-12-09 00:57:00 +0900

    セミコロン抜け修正

--- app/Article.php
+++ app/Article.php
@@ -8,7 +8,7 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Article extends Model
 {
     public function user(): BelongsTo{
-        return $this->belongsTo('App/User')
+        return $this->belongsTo('App/User');
     }
     
 }
