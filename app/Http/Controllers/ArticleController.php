dd39da85caecc77e8efa4060d5303198d50ba89e
shimamatz@gmail.com <shimamatz@gmail.com>
2021-12-22 03:58:42 +0900

    記事投稿機能の作成

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-use App\Article;
 
+use App\Article;
+use App\Http\Requests\ArticleRequest;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -12,7 +13,16 @@ class ArticleController extends Controller
         return view('articles.index', ['articles' => $articles]);
     }
 
-    public function create(){
-        return view('articles.create');
+    public function create()
+    {
+        return view('articles.create');    
+    }
+
+    public function store(ArticleRequest $request, Article $article)
+    {
+        $article->user_id = $request->user()->id;
+        $article->fill($request->all());
+        $article->save();
+        return redirect()->route('articles.index');
     }
 }
